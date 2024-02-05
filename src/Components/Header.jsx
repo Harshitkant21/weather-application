38c0646218a590620a9e77cd6b39f6df0f16e3b9
Harshitkant21 <harshit0576.be21@chitkara.edu.in>
2024-02-06 03:22:43 +0530

    navbar added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,48 @@
-import React from "react";
-import './Header.css';
+import React, { useState } from "react";
+import "../CSS/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import logo_white from "../assests/logo_white.png";
+import logo_dark from "../assests/logo.png";
 
-const Header = () => {
+const Header = ({ theme, setTheme }) => {
+  const toggle = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
   return (
     <>
-      <h1>hello</h1>
+      <div className={`navbar ${theme}`}>
+        <img
+          src={theme === "light" ? logo_dark : logo_white}
+          alt="Logo here"
+          className="logo"
+        />
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Features</li>
+          <li>About</li>
+        </ul>
+
+        <div className="search-box">
+          <input type="text" placeholder="Search..." />
+          <FontAwesomeIcon
+            icon={faSearch}
+            className={theme === "light" ? "icon_light" : "icon_dark"}
+          />
+        </div>
+
+        <FontAwesomeIcon
+          icon={theme === "light" ? faSun : faMoon}
+          className="toggle-icon"
+          onClick={() => toggle()}
+        />
+      </div>
     </>
   );
 };
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
+import "../CSS/Home.css";
 
 const Home = () => {
+  const [theme, setTheme] = useState("light");
   return (
     <>
-      <div className="container">
-        <Header />
+      <div className={`container ${theme}`}>
+        <Header theme={theme} setTheme={setTheme}/>
       </div>
     </>
   );
